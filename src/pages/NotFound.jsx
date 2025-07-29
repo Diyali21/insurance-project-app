30ff1bb653a0acb409b75364fab9395839e5f3f0
Diyali21 <Diyali.Devraj@sanlam.co.za>
2025-07-29 10:03:49 +0200

    not found page using mui

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,14 @@
+import { Box } from "@mui/material";
+
 export function NotFound(){
     return (
-        <div className="not-found-container">
-            <img className="not-found" src="https://www.shutterstock.com/image-vector/404-page-not-found-error-600nw-685240003.jpg" alt="" />
-        </div>
+        <Box className="not-found-container">
+        <Box
+        component="img"
+        src="https://www.shutterstock.com/image-vector/404-page-not-found-error-600nw-685240003.jpg"
+        alt="not found"
+        className="not-found">
+        </Box>
+        </Box>
     )
 }
